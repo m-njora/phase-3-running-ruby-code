a1f1abf7fe3e82d0a25dbc95e98c526daeae30e7
m-njora <mmuthekijora@gmail.com>
2023-02-07 08:22:45 +0300

    done

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 # pp [{ id: 1, hello: "World" }, 
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
+
 #     { id: 2, hello: "Ruby" }, 
 #     { id: 3, hello: "Moon" }, 
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
 
 # def add
     
